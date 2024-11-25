d9f8f6b150a9338a08cfa69e1f0f6c19825039cb
rodrigolugon <rodrigolugonn@gmail.com>
2024-11-25 14:38:23 -0300

    tentativas - 1

--- jogo/jogo.c
+++ jogo/jogo.c
@@ -1,6 +1,6 @@
+// Mostre em qual tentativa o usuario acertou. while == 0
 #include <stdio.h>
 #include <locale.h>
-#define NUMERO_DE_TENTATIVAS 3
 
 int main() {
     setlocale(LC_ALL, "portuguese");
@@ -11,29 +11,31 @@ int main() {
 
     int numerosecreto = 42;
     int chute;
+    int ganhou = 0;
+    int tentativas = 1;
 
-    for (int i = 1; i <= NUMERO_DE_TENTATIVAS; i++) {
+    while (ganhou == 0) { //enquanto ganhou = a nao ganhou repete o loop
+        //usamos while quando nao sabemos quando o loop vai acabar
 
-        printf("Tentativa %d de %d\n", i, NUMERO_DE_TENTATIVAS);
+
+        printf("Tentativa %d\n", tentativas);
         printf("Qual é o seu chute? ");
         scanf_s("%d", &chute);
         printf("Seu chute foi %d\n", chute);
 
         if (chute < 0) {
             printf("Você não pode chutar números negativos!\n");
-            i = i - 1; //i--
-            continue; // continua o bloco sem matar o loop
+            continue;
         }
 
         int acertou = (chute == numerosecreto);
         int maior = chute > numerosecreto;
-        int menor = chute < numerosecreto;
 
         if (acertou) {
 
             printf("Parabéns! Você acertou!\n");
             printf("Jogue de novo, você é um bom jogador!\n");
-            break;
+            ganhou = 1; //para o loop quando for verdadeiro
         }
 
         else if (maior) {
@@ -42,14 +44,15 @@ int main() {
 
         else {
             printf("Seu chute é menor que o número secreto\n");
-
         }
 
+        tentativas++; //atencao
     }
 
-    printf("Fim de jogo");
+    printf("Fim de jogo\n");
+    printf("Você acertou em %d tentativas!\n", tentativas - 1);
+    //experimente tirar o -1
 
     return 0;
 
 }
-
