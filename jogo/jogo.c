508ad538f368c5adf642c008f68f3ec7ce526bca
rodrigolugon <rodrigolugonn@gmail.com>
2024-11-25 13:35:19 -0300

    A variavel é visivel somente no {escopo} em que foi declarada.

--- jogo/jogo.c
+++ jogo/jogo.c
@@ -1,4 +1,3 @@
-//O usuario precisa ter 3 tentativas.
 #include <stdio.h>
 #include <locale.h>
 
@@ -12,33 +11,25 @@ int main() {
     int numerosecreto = 42;
     int chute;
 
-    for (int i = 1; i <= 3; i++) {
+    printf("Qual é o seu chute? ");
+    scanf_s("%d", &chute);
+    printf("Seu chute foi %d\n", chute);
 
-        printf("Tentativa %d de 3\n", i);
-        printf("Qual é o seu chute? ");
-        scanf_s("%d", &chute);
-        printf("Seu chute foi %d\n", chute);
-
-        int acertou = (chute == numerosecreto);
-        if (acertou) {
-
-            printf("Parabéns! Você acertou!\n");
-            printf("Jogue de novo, você é um bom jogador!\n");
-
+    int acertou = (chute == numerosecreto);
+    if (acertou) {
+        printf("Parabéns! Você acertou!\n");
+        printf("Jogue de novo, você é um bom jogador!\n");
+    }
+    else {
+        int maior = chute > numerosecreto;
+        if (maior) {
+            printf("Seu chute é maior que o número secreto\n");
         }
         else {
-            int maior = chute > numerosecreto;
-            if (maior) {
-                printf("Seu chute é maior que o número secreto\n");
-            }
-            else {
-                printf("Seu chute é menor que o número secreto\n");
-            }
+            printf("Seu chute é menor que o número secreto\n");
         }
     }
-    printf("Fim de jogo");
 
     return 0;
-
 
 }
